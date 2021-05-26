5aaab6ab0becde085887c0ae28d368ea87a35334
achyutjagini <achyut.jagini@gmail.com>
2021-05-26 21:06:26 +0530

    Add files via upload

---
+++ build/precache-manifest.4fc2639bd5c1d23638fef9942c96ee94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a50d36b582bf3528b3db877cc8f559",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c686e7dac61771b36bb2",
+    "url": "/static/css/main.b3209779.chunk.css"
+  },
+  {
+    "revision": "36128ff7f373e5beff7a",
+    "url": "/static/js/2.1442e098.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.1442e098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c686e7dac61771b36bb2",
+    "url": "/static/js/main.74cb5c81.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
